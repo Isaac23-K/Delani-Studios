bc58f131d52781bf5c848fbc287cdec3e33d3ce8
Isaac23-K <ikaptoge@yahoo.com>
2020-02-23 22:22:14 +0300

    Made additional changes to the html

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(".click0").click(function(){
+  $(".hide0").slideToggle(1000);
+  $(".show0").slideToggle(1000);
+})
+
+$(".click1").click(function(){
+$(".hide").slideToggle(1000);
+$(".show").slideToggle(1200);
+})
+
+$(".select").click(function(){
+$(".hide3").slideToggle(1000);
+$(".show3").slideToggle(1200);
+})
