8dda75bb7c82e4168277e1dbe2ff110346e10976
Isaac23-K <ikaptoge@yahoo.com>
2020-02-26 09:38:11 +0300

    add mailchip

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
-$(".click0").click(function(){
-  $(".hide0").slideToggle(1000);
-  $(".show0").slideToggle(1000);
+$("#click0").click(function(){
+  $(".hide0").Toggle(1000);
 })
 
 $(".click1").click(function(){
 $(".hide").slideToggle(1000);
-$(".show").slideToggle(1200);
 })
 
 $(".select").click(function(){
 $(".hide3").slideToggle(1000);
-$(".show3").slideToggle(1200)
 })
-
